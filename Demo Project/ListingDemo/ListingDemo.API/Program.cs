ea34e2d7a3f729246e160cdb28f2c81f7b3fda25
rahul-aravinth <rahularavinth21@gmail.com>
2022-07-26 21:58:40 +0530

    Adding Serilog and Seq

--- Demo Project/ListingDemo/ListingDemo.API/Program.cs
+++ Demo Project/ListingDemo/ListingDemo.API/Program.cs
@@ -1,3 +1,5 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -11,6 +13,9 @@ builder.Services.AddCors(services =>
 {
     services.AddPolicy("AllowAll", x => x.AllowAnyHeader().AllowAnyOrigin().AllowAnyOrigin());
 });
+
+// ctx - context here refers 'builder', lc - logger configuration
+builder.Host.UseSerilog((ctx, lc) => lc.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration));
 
 var app = builder.Build();
 
