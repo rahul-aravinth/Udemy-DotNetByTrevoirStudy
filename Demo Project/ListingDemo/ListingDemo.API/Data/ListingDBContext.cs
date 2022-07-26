f0db9446c86f755dd2e3fd0d9fb0fb7d8a178bdb
rahul-aravinth <rahularavinth21@gmail.com>
2022-07-26 23:56:51 +0530

    Code First Migration Changes

---
+++ Demo Project/ListingDemo/ListingDemo.API/Data/ListingDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ListingDemo.API.Data
+{
+    public class ListingDBContext : DbContext
+    {
+        public ListingDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Country> Countries { get; set; }
+    }
+}
